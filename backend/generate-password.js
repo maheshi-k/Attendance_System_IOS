fd573907b8a73faa6ad18801f431974f44fd3cb1
thilini-maheshika <kthilini1999@gmail.com>
2026-09-04 15:11:08 +0530

    remove unused files

--- backend/generate-password.js
+++ backend/generate-password.js
@@ -1,8 +1,8 @@
-import bcrypt from "bcryptjs";
+// import bcrypt from "bcryptjs";
 
-const password = "Admin@123";
+// const password = "Admin@123";
 
-const hash = await bcrypt.hash(password, 10);
+// const hash = await bcrypt.hash(password, 10);
 
-console.log("Password:", password);
-console.log("Hash:", hash);
+// console.log("Password:", password);
+// console.log("Hash:", hash);
